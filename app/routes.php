b578a4edbbc6fb8dde631163490187ea798c64ab
Peter Davey <pdavey@jj-graphiks.de>
2014-09-23 21:41:39 +0200

    Test

--- app/routes.php
+++ app/routes.php
@@ -8,12 +8,13 @@
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
+| some more tesxt here
 |
 */
 
 Route::get('users', function()
 {
-	return Response::view('hello');
+	return Response::view('hello test text');
 });
 
 
